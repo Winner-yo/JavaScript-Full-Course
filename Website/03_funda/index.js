5d38ef033708692544fb21899a6b633b501ab342
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-22 00:49:53 +0300

    updated

--- Website/03_funda/index.js
+++ Website/03_funda/index.js
@@ -1,8 +1,20 @@
 //Math = built-in object that provides a collection and method
 console.log(Math.PI, Math.E);
 let value1 = 3.6
+let x = 3;
+let y = 2;
+let z = 60;
 let newValue = Math.round(value1);// to the nearst higher or lower number
-let newValue1 = Math.floor(value1);//rounded down 
+let newValue1 = Math.floor(value1);//no matter how rounded down 
+let newValue2 = Math.ceil(value1);//no matter how rounded up 
+let newValue3 = Math.trunc(value1);//eliminate decimal part
+let newValue4=Math.pow(x,y)
+let newValue5=Math.sqrt(z)
+let newValue6=Math.sin(z)
+let newValue7=Math.cos(z)
+let newValue8=Math.tan(z)
+let newValue9 = Math.log(10)
+let maximum=Math.max(x,y,z)
 
 console.log(newValue);
 console.log()
