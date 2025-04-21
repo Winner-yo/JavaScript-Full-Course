6e9cc0eda8c00602d910fdab74e71fc1a42fab8d
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-22 00:39:12 +0300

    updated

---
+++ Website/03_funda/index.js
@@ -0,0 +1,8 @@
+//Math = built-in object that provides a collection and method
+console.log(Math.PI, Math.E);
+let value1 = 3.6
+let newValue = Math.round(value1);// to the nearst higher or lower number
+let newValue1 = Math.floor(value1);//rounded down 
+
+console.log(newValue);
+console.log()
