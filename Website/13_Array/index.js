bf6c8a027e9e439c29b49675382f50e9aedb722b
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-23 23:56:24 +0300

    array change

---
+++ Website/13_Array/index.js
@@ -0,0 +1,10 @@
+//Array = variable like structure that hold more than 1 value
+let fruits = ['apple', 'banana', 'orange'];
+console.log(fruits[0]);//accessing individual element
+
+fruits[1] = 'coconut'//changing the value
+fruits[3] = 'mango'//adding element since last index is 2
+fruits.push()
+console.log(fruits)
+
+
