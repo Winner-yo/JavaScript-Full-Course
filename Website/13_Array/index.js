ac3f8b38b551c650888932e384d9917c03bb0408
Ashenafi Damena <ashenafidamena415@gmail.com>
2025-04-24 10:03:19 +0300

    array done

--- Website/13_Array/index.js
+++ Website/13_Array/index.js
@@ -4,7 +4,24 @@ console.log(fruits[0]);//accessing individual element
 
 fruits[1] = 'coconut'//changing the value
 fruits[3] = 'mango'//adding element since last index is 2
-fruits.push()
+fruits.push('pinapple')//adding to the end
+fruits.unshift("lomen")//adding to the beginning of an array
+fruits.shift()//removing from beginning
+fruits.pop()//this will remove the last element
+console.log(fruits.indexOf('mango'))
+console.log(fruits.indexOf('potato'))//if not exist in the array it will return -1
+let numOfFruits = fruits.length;
+console.log(numOfFruits)
 console.log(fruits)
-console.log(fruits[-1])
-
+//console.log(fruits[-5])
+for (let i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+}
+for (let i = fruits.length-1; i >= 0; i--){  //this will reverse the value
+    console.log(fruits[i]);
+}
+fruits.sort()//sorting ascending order
+fruits.sort().reverse()//reversing order
+for (let fruit of fruits){  //shortcut for above
+    console.log(fruit)
+}
